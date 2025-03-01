fb86c85b4036f832eb1a51d8cd0f3e8ead0f5fad
Bruno <86791361+Bruno514@users.noreply.github.com>
2025-03-01 09:48:53 -0300

    add navbar

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,7 @@
 const messages = require("../models/messageModel");
 
 function get(req, res) {
-  res.render("index", { title: "Mini Messaging Board", messages: messages });
+  res.render("index", { title: "Message Board", messages: messages });
 }
 
 module.exports = { get };
