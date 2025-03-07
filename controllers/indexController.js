ce28281fb08844b467d62427ce0424cd48406ef3
Bruno <86791361+Bruno514@users.noreply.github.com>
2025-03-07 13:56:18 -0300

    fix date format and make responsive on smalller phones

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,11 +1,13 @@
 const db = require("../db/queries");
+const { format } = require("date-fns");
 
 async function get(req, res) {
   const messages = await db.getAllMessages();
-  
+
   res.render("index", {
     title: "Message Board",
     messages: messages,
+    format: format,
   });
 }
 
--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,14 +1,16 @@
 const db = require("../db/queries");
+const { format } = require("date-fns");
 
 async function get(req, res) {
   const id = req.query.id;
 
   const message = await db.getMessage(id);
-  console.log(message)
+  console.log(message);
 
   res.render("message", {
     title: "Message details",
     message: message[0],
+    format: format,
   });
 }
 
